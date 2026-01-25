36be610473e00a0406a818874a74f475fc82412e
mordfan <mordfan@gmail.com>
2026-01-25 19:49:04 +0100

    cleaning up a lil bit

--- 2025/10/p2/main.py
+++ 2025/10/p2/main.py
@@ -1,93 +1,95 @@
 #!/bin/python3
 # https://adventofcode.com/2025/day/10#part2
+# amazing idea of bifurcatin from r/tenthmascot since all of mine ideas failed...
 
 from os import path
+from collections import Counter
 from collections.abc import Iterator
-from math import pow
+from functools import cache
 from itertools import chain, combinations
-from functools import reduce, cache
+from dataclasses import dataclass, field
 
 
-def get_data(file_name: str) -> Iterator[tuple[str, list[list[int]], list[int]]]:
+def get_key_repr(coverage: list[bool]) -> str:
+    return ''.join(['1' if c else '0' for c in coverage])
+
+
+@dataclass(frozen=True, eq=False, order=False)
+class Combo:
+    key: tuple[bool, ...] = field(repr=False)
+    key_repr: str = field(init=False)
+    buttons: list[tuple[int, ...]] = field()
+    counters: Counter = field()
+
+    def __post_init__(self) -> None:
+        object.__setattr__(self, "key_repr", get_key_repr(self.key))
+
+    def __lt__(self, other) -> bool:
+        return self.key == other.key and len(self.buttons) < len(other.buttons)
+
+
+def get_data(file_name: str) -> Iterator[tuple[list[tuple[int, ...]], tuple[int, ...]]]:
+    """:return: tuple[list[wiring: tuple[int, ...]], joltage: tuple[int, ...]]"""
     with open(path.join('..', 'data', file_name), mode='r', encoding='utf-8') as file:
         while (line := file.readline().rstrip()):
-            assert line.startswith('['), "invalid begining of line"
-
-            # light diagram
-            b = 0
-            e = line.find(']', b)
-            assert e > b, "could not locate light diagram"
-            ld = line[b + 1:e]
-
-            # button wirings
-            b = e + 1
-            e = line.find('{', b)
-            assert e > b, "could not locate button wiring"
-            bw = list([list(list(map(int, x.split(',')))) for x in line[b + 1: e - 2].lstrip('(').rstrip(')').split(') (')])
-
-            # joltage requirements
-            b = e + 1
-            e = line.find('}', b)
-            assert e > b, "could not locate joltage requirements"
-            jr: list[int] = list(map(int, line[b:e].split(',')))
-
-            # return
-            yield (ld, bw, jr)
+            _, *wiring, joltage = line.split()
+            yield ([tuple(map(int, w[1:-1].split(','))) for w in wiring], tuple(map(int, joltage[1:-1].split(','))))
 
 
-def list_to_bin(data: list[int]) -> int:
-    return int(sum([(1 * pow(2, i) if i in data else 0) for i in range(0, max(data) + 1)]))
+def get_button_combos(wiring: list[tuple[int, ...]], joltage: tuple[int, ...]) -> dict[str, Combo]:
+    """Returns dictionary where:
+        :key: is a string representing joltage coverage,
+        :value: is Combo dataclass"""
+    ret: dict[str, Combo] = {}
 
-
-@cache
-def get_combos(length: int) -> list[list[int]]:
-    s = list(range(length))
-    combos = list(chain.from_iterable(combinations(s, r) for r in range(1, length + 1)))
-    return combos
-
-
-def find_best_combo(joltage: list[int], wiring: list[list[int]]) -> int | None:
-    print(f"{joltage=})\n{wiring=}")
-    counters = {i: x for i, x in enumerate(joltage) if x > 0}
-    ret: int | None = None
-    length = len(joltage)
-    fmt = f"{{:0{length}b}}"
-    joltage_bin = list_to_bin([i for (i, n) in enumerate(joltage) if n > 0])
-    wiring_bin = [list_to_bin(w) for w in wiring]
-    print(f"wiring_bin={[fmt.format(w) for w in wiring_bin]}")
-    print(f"joltage_bin={fmt.format(joltage_bin)}")
-
-    for combos_indices in get_combos(len(wiring)):
-        # checking if combination covers joltage
-        combos_bin = reduce(lambda t, c: t | wiring_bin[c], combos_indices, 0)
-        if combos_bin != joltage_bin:
-            continue
-        combos = [wiring[c] for c in combos_indices]
-        print(f"  combos={fmt.format(combos_bin)} ({'good' if combos_bin == joltage_bin else 'bad'}) {combos}")
-
-        # todo: now need to figure out a way to calculate best solution
-        cnt = counters.copy()
-        total = 0
-        x = get_combos(len(combos_indices))
-
-        # for c in x:  # sorted(combos, key=lambda x: len(wiring[x]), reverse=True):
-        #     print(f"    c={c}")
-
-        if total > 0 and (ret is None or ret > total):
-            ret = total
+    for wiring_indices in chain.from_iterable(combinations(range(len(wiring)), r) for r in range(1, len(wiring) + 1)):
+        buttons = tuple([wiring[i] for i in wiring_indices])
+        counters = Counter(chain.from_iterable(buttons))
+        if any([x > 1 for x in counters.values()]):
+            continue  # that eliminates cases where one wire occurs multiple times
+        coverage = tuple([counters.get(x, 0) % 2 != 0 for x in range(len(joltage))])
+        combo = Combo(coverage, buttons, counters)
+        if combo.key not in ret.keys():
+            ret[combo.key] = combo
+            print(f"  [+] {combo}")
+        elif ret[combo.key] > combo:
+            print(f"  [v] {ret[combo.key]}")
+            ret[combo.key] = combo
+            print(f"  [^] {combo}")
 
     return ret
 
 
-def main(file_name: str) -> None:
-    total: int = 0
-    for line, (_, wiring, joltage) in enumerate(get_data(file_name)):
-        n = find_best_combo(joltage, wiring)
-        # assert n is not None, f"no solution for {line=}"
-        print('---')
-        if n is not None:
-            total += n
+def find_best_combo(wiring: list[tuple[int, ...]], joltage: tuple[int, ...]) -> int | None:
+    print(f"{joltage=}, {wiring=}")
+    combos = get_button_combos(wiring, joltage)
+    presses: dict[tuple[int, ...], int] = {w: 0 for w in wiring}  # key=button/wiring, value=number of presses
+    counters: list[int] = list(joltage)  # copy of joltage
 
+    covered = True
+    for n in sorted(set(joltage), reverse=True):
+        coverage = get_key_repr([joltage[i] >= n for i in range(len(joltage))])
+        # print(f"{n=}, {coverage=}")
+        if coverage not in combos.keys():
+            print(f"  ..no elligible {coverage=} for {n=}")
+            covered = False
+
+    if not covered:
+        # for c in sorted(combos):
+        #     print(f"  {c=}, {combos[c]=}")
+        return 0
+    else:
+        return 1
+
+
+def main(file_name: str) -> None:
+    total = 0
+    for line, (wiring, joltage) in enumerate(get_data(file_name)):
+        n = find_best_combo(wiring, joltage)
+        # assert n is not None, "no good combo found"
+        # assert n > 0, "does that suppose to happen?"
+        # total += n
+        break
     print(f"{total=:,}")
 
 
